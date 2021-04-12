4e254f9b55536205fdaee958a5a23197896c1ca7
con-f-use <con-f-use@gmx.net>
2021-04-12 13:38:49 +0200

    docs: get release from package

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
+
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath("../src/"))
+import printingpress
 
 
 # -- Project information -----------------------------------------------------
@@ -22,7 +25,7 @@ copyright = "2021, confus"
 author = "confus"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3"
+release = printingpress.__version__
 
 
 # -- General configuration ---------------------------------------------------
